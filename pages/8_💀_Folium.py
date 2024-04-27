a5890080b4671393dac0cfd23e190d78d876e088
Brycen Harris <bharri94@vols.utk.edu>
2024-04-26 21:50:37 -0400

    add folimp

---
+++ pages/8_💀_Folium.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import geojp.foliumap as geojp
+
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("Geojp Map")
+
+with st.expander("See source code"):
+    with st.echo():
+        m = geojp.Map()
+        m.add_basemap("OpenTopoMap")
+
+m.to_streamlit(widtd=1000, height=700)
